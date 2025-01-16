685930d448ca0372cf8df6e779c1bda22caec287
Mpstme Student <mpstme.student@SVKMGRP.COM>
2025-01-16 14:21:44 +0530

    Folderizing and pushing alL lAB 2 Android Studio build files as exported

---
+++ LAB 2/K036_LAB2_MAD/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "K036_LAB2_MAD"
+include(":app")
+ 
